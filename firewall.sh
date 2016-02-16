8a1ff7a0c47619a23cd333e4799d4d5f3eddfb8c
galiper34 <galiper34@gmail.com>
2016-02-16 21:20:35 +0100

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -118,20 +118,54 @@ fw_stop(){
         echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter
         echo 0 > /proc/sys/net/ipv4/tcp_syncookies
 }
+
+fw_stop(){
+        # Vidage des tables et des regles personnelles
+        iptables -t filter      -F
+        iptables -t nat         -F
+        iptables -t mangle      -F
+        iptables -t filter      -X
+ 
+        # Interdire toutes connexions entrantes et sortantes
+        iptables -t filter      -P INPUT STOP
+        iptables -t filter      -P FORWARD STOP
+        iptables -t filter      -P OUTPUT STOP
+        
+        echo 0 > /proc/sys/net/ipv4/ip_forward
+        echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_all
+        echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+        echo 0 > /proc/sys/net/ipv4/icmp_ignore_bogus_error_responses
+        echo 1 > /proc/sys/net/ipv4/conf/all/accept_source_route
+        echo 0 >/proc/sys/net/ipv4/conf/all/log_martians
+        echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter
+        echo 0 > /proc/sys/net/ipv4/tcp_syncookies
+        
+        #Regle pour garder SSH
+        iptables -A INPUT -i $EXTERNE -m state --state RELATED,ESTABLISHED -j ACCEPT
+        iptables -A OUTPUT -i $EXTERNE -m state --state RELATED,ESTABLISHED -j ACCEPT
+        # Autoriser SSH
+        iptables -t filter -A INPUT -i $EXTERNE -p tcp --dport $SSH_PORT -j ACCEPT
+        iptables -t filter -A OUTPUT -i $EXTERNE -p tcp --dport $SSH_PORT -j ACCEPT
+}
  
 case "$1" in
         start|restart)
-                echo -n "Starting firewall.."
+                echo -n "Firewall START"
                 fw_start
                 echo "Firewall [ON]"
                 ;;
         stop)
-                echo -n "Stopping firewall.."
+                echo -n "Firewall STOP"
                 fw_stop
                 echo "Firewall [OFF]"
                 ;;
+        pause)
+                echo -n "Firewall PAUSE"
+                fw_stop
+                echo "Firewall [PAUSE]"
+                ;;                
         *)
-                echo "Usage: $0 {start|stop|restart}"
+                echo "Usage: $0 {start|stop|pause|restart}"
                 exit 1
                 ;;
 esac
